98085b7f3c8fa5303fbb7f9720c0b8761cbebd22
Sadok3060 <sadokjerbi3060@gmail.com>
2025-10-27 00:23:17 +0100

    ci-cd

--- public/index.php
+++ public/index.php
@@ -2,7 +2,14 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+set_error_handler(function ($severity, $message, $file, $line) {
+    if (str_contains($message, 'ini_set(): assert.warning INI setting is deprecated')) {
+        return true;
+    }
+    return false;
+});
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
